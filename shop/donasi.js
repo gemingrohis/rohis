7f9695c77b0672bf393b2f1bbb9b850e180e4a42
gemingrohis <97324763+gemingrohis@users.noreply.github.com>
2022-01-07 19:20:59 -0800

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -1,27 +1,19 @@
 const donasibot = () => {
 return `DONASI BISA VIA BANK / E-MONEY
 
-*🏧 BANK*
-
-1. BRI 
-	ㅁ 8881 0887 4350 47326
-2. BCA 
-	ㅁ 3901 0887 4350 47326
-3. BNI 
-	ㅁ 881 0887 4350 47326
-4. PERMATA
-	ㅁ 852 8887 4350 47326
-	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0882 1246 5570
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 0882 1246 5570
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0882 1246 5570
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama R**I
+
+JOIN GRUP NULL OFFICIAL:
+https://chat.whatsapp.com/Hl4KQuVJrZP0OtBmcdF9CV
 `
 	}
 
